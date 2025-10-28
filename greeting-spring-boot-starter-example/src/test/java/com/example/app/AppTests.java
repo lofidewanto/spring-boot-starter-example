cc030234fa1d99716c338c01899ee8cec4bc488b
lofidewanto <lofidewanto@gmail.com>
2025-10-29 00:29:45 +0100

    Add everything...

--- greeting-spring-boot-starter-example/src/test/java/com/example/app/AppTests.java
+++ greeting-spring-boot-starter-example/src/test/java/com/example/app/AppTests.java
@@ -1,13 +1,23 @@
 package com.example.app;
 
+import com.example.greeting.GreetingService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AppTests {
 
+	@Autowired
+	GreetingService greetingService;
+
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void testGreetingService() {
+		String greeting = greetingService.greet("World");
+		assert(greeting.equals("Hello, World!"));
+	}
 }
