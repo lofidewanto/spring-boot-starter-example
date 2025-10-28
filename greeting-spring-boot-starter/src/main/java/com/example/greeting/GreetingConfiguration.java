566299139c63cc186b9cd32571f55bebd62a6edd
lofidewanto <lofidewanto@gmail.com>
2025-10-29 00:45:26 +0100

    Add everything...

--- greeting-spring-boot-starter/src/main/java/com/example/greeting/GreetingConfiguration.java
+++ greeting-spring-boot-starter/src/main/java/com/example/greeting/GreetingConfiguration.java
@@ -1,17 +1,22 @@
 package com.example.greeting;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnClass(GreetingService.class)
 public class GreetingConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean
     public GreetingService greetingService() {
         return new GreetingService();
     }
 
     @Bean
+    @ConditionalOnMissingBean
     public LoggingAspect loggingAspect() {
         return new LoggingAspect();
     }
